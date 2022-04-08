fef0948d3b3dc158015b7635056cb87a9b8cc60d
Kilmer Luiz <kilmerluizale@gmail.com>
2022-04-08 12:11:19 -0300

    Update tweets index to sort and paginate.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,15 @@
 class TweetsController < ApplicationController
   def index
-    render json: Tweet.all
+    render json: Tweet.order('created_at DESC').limit(5).offset(page_offset)
+  end
+
+  private
+
+  def page_offset
+    (tweets_params[:page].to_i - 1) * 5
+  end
+
+  def tweets_params
+    params.permit(:page)
   end
 end
--- spec/requests/tweet_request_spec.rb
+++ spec/requests/tweet_request_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Tweets", type: :request do
 
+  before do
+    2.times do
+      company = Company.create(name: Faker::Company.name)
+      2.times do |index|
+        user = User.create(display_name: Faker::Name.name, email: Faker::Internet.email, username: "user_#{company.id}_#{index+1}", company_id: company.id)
+        10.times do
+          Tweet.create(body: Faker::GreekPhilosophers.quote, user_id: user.id)
+        end
+      end
+    end
+
+  end
+
+  describe '#index' do
+    let(:result) { JSON.parse(response.body) }
+    let(:params) { { page: 1 } }
+
+    it 'return sorted by created_at and paginated tweets' do
+      get tweets_path(params)
+
+      expect(result.size).to eq(5)
+      expect(result.first['id']).to eq Tweet.order('created_at DESC').first.id
+    end
+  end
 end
